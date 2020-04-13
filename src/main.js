1d052931d00bca5ef50161ff0b9a385cf176a6da
ouyangshan09 <ouyangshan09@gmail.com>
2020-04-13 23:10:49 +0800

    feat: 添加标装库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
 import './plugins/element.js'
+import 'normalize.css/normalize.css'
 
 Vue.config.productionTip = false
 
