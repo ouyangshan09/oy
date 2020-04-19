ea96bbe19ee8b9946d3f51fbae2307ae7b6d4c89
ouyangshan09 <ouyangshan09@gmail.com>
2020-04-20 01:27:22 +0800

    style: 页面横向滚动条

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -132,7 +132,7 @@ export default {
 <style lang="scss" scoped>
 .tags-view-container {
   height: 35px;
-  width: 100%;
+  // width: 100%;
   background: #fff;
   border: 1px solid #d8dce5;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 0 3px 0 rgba(0, 0, 0, 0.04);
