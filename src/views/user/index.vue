f9ed74c4c667abb5ee297f7294806aea9a160ee5
ouyangshan09 <ouyangshan09@gmail.com>
2020-05-05 15:21:27 +0800

    style: UI

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="user-container">
     <div class="filter-condition">
-      过滤条件
+      <el-input
+        class="filter-item"
+        v-model="query.username"
+        placeholder="请输入用户名"
+        style="width: 140px"
+        size="small"
+        @keyup.enter.native="handleSearch"
+        clearable
+      />
+      <el-select
+        class="filter-item"
+        v-model="query.status"
+        placeholder="用户状态"
+        style="width: 120px"
+        size="small"
+        clearable
+      >
+        <el-option
+          v-for="item in statusList"
+          :label="item.label"
+          :value="item.id"
+          :key="item.id"
+        />
+      </el-select>
+      <el-button
+        class="filter-item"
+        icon="el-icon-search"
+        type="primary"
+        size="small"
+        @click="handleSearch"
+      >
+        搜索
+      </el-button>
     </div>
     <el-table :data="rows" border stripe>
       <el-table-column align="center" label="ID">
@@ -49,6 +81,7 @@
 
 <script>
 import Pagination from '../../components/Pagination'
+import { cleanJson } from '../../utils'
 
 const defaultForm = {
   id: "",
@@ -73,6 +106,10 @@ export default {
         status: '',
         createAt: '',
       },
+      statusList: [
+        { id: 1, label: '启用' },
+        { id: 2, label: '禁用' },
+      ],
       loading: false,
       form: {
         id: "",
@@ -98,6 +135,9 @@ export default {
     },
     handlePagination(query) {
       console.log('pagination query:', query)
+    },
+    handleSearch() {
+      console.log('query:', cleanJson(this.query))
     }
   }
 };
